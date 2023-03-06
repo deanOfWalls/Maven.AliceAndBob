3996ac89720006d2527a383ed6e6a41b3114d06e
deanOfWalls <deanofwalls@gmail.com>
2023-03-06 17:31:50 -0500

    completed exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,27 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        String alice = "Alice";
+        String bob = "Bob";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+        if(name.equals(alice) || name.equals(bob)){
+            System.out.println("Hello, " + name + "!");
+        }
+        else{
+            System.out.println("I don't know you," + name + ".");
+        }
+
+
+
+
 
     }
 }
